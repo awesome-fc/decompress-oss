3b5cf4d86771c04b2366ecd9d0187111487718c4
rsonghuster <ls_huster@163.com>
2021-12-21 14:40:16 +0800

    add enhancedInstance example

--- functions/archvie/tar/index.py
+++ functions/archvie/tar/index.py
@@ -3,18 +3,23 @@ import tarfile
 import oss2
 from oss2 import utils, models
 import logging
-import os, json, time
+import os
+import json
+import time
 from helper import LOGGER, OssStreamFileLikeObject
 
 # a decorator for print the excute time of a function
+
+
 def print_excute_time(func):
     def wrapper(*args, **kwargs):
         local_time = time.time()
         ret = func(*args, **kwargs)
         LOGGER.info('current Function [%s] excute time is %.2f' %
-              (func.__name__, time.time() - local_time))
+                    (func.__name__, time.time() - local_time))
         return ret
     return wrapper
+
 
 @print_excute_time
 def handler(event, context):
@@ -36,9 +41,9 @@ def handler(event, context):
     if "ObjectCreated:PutSymlink" == evt['eventName']:
         object_name = bucket.get_symlink(object_name).target_key
     if object_name == "":
-      raise RuntimeError('{} is invalid symlink file'.format(
-          evt['oss']['object']['key']))
-    
+        raise RuntimeError('{} is invalid symlink file'.format(
+            evt['oss']['object']['key']))
+
     lst = object_name.split("/")
     file_name = lst[-1]
     PROCESSED_DIR = os.environ.get("PROCESSED_DIR", "")
@@ -49,19 +54,19 @@ def handler(event, context):
     if object_name.endswith(".tar.gz"):
         open_mode = "r:gz"
         newKey = newKey.replace(".tar.gz", "/")
-    
+
     elif object_name.endswith(".tgz"):
         open_mode = "r:gz"
         newKey = newKey.replace(".tgz", "/")
-    
+
     elif object_name.endswith(".tar.bz2"):
         open_mode = "r:bz2"
         newKey = newKey.replace(".tar.bz2", "/")
-    
+
     elif object_name.endswith(".tar.xz"):
         open_mode = "r:xz"
         newKey = newKey.replace(".tar.xz", "/")
-        
+
     elif object_name.endswith(".tar"):
         open_mode = "r:"
         newKey = newKey.replace(".tar", "/")
@@ -69,9 +74,9 @@ def handler(event, context):
     else:
         raise RuntimeError(
             '{} filetype is not in  [".tar.gz", ".tgz", ".tar.bz2", ".tar.xz", ".tar"]'.format(object_name))
-        
+
     LOGGER.info("start to decompress tar file = {}".format(object_name))
-    
+
     oss_file_obj = OssStreamFileLikeObject(bucket, object_name)
     with tarfile.open(fileobj=oss_file_obj, mode=open_mode) as tf:
         for entry in tf.getmembers():
--- functions/archvie/zip/index.py
+++ functions/archvie/zip/index.py
@@ -21,8 +21,10 @@ If necessary to rewrite this function, and ensure that the debugging pass
 '''
 
 import helper
-import oss2, json
-import os, time
+import oss2
+import json
+import os
+import time
 import logging
 import chardet
 
@@ -40,78 +42,82 @@ logging.getLogger("oss2.auth").setLevel(logging.ERROR)
 LOGGER = logging.getLogger()
 
 # a decorator for print the excute time of a function
+
+
 def print_excute_time(func):
     def wrapper(*args, **kwargs):
         local_time = time.time()
         ret = func(*args, **kwargs)
         LOGGER.info('current Function [%s] excute time is %.2f' %
-              (func.__name__, time.time() - local_time))
+                    (func.__name__, time.time() - local_time))
         return ret
     return wrapper
 
+
 @print_excute_time
 def handler(event, context):
-  """
-  The object from OSS will be decompressed automatically .
-  param: event:   The OSS event json string. Including oss object uri and other information.
-      For detail info, please refer https://help.aliyun.com/document_detail/70140.html?spm=a2c4g.11186623.6.578.5eb8cc74AJCA9p#OSS
-  
-  param: context: The function context, including credential and runtime info.
+    """
+    The object from OSS will be decompressed automatically .
+    param: event:   The OSS event json string. Including oss object uri and other information.
+        For detail info, please refer https://help.aliyun.com/document_detail/70140.html?spm=a2c4g.11186623.6.578.5eb8cc74AJCA9p#OSS
 
-      For detail info, please refer to https://help.aliyun.com/document_detail/56316.html#using-context
-  """
-  evt_lst = json.loads(event)
-  creds = context.credentials
-  auth=oss2.StsAuth(
-    creds.access_key_id,
-    creds.access_key_secret,
-    creds.security_token)
+    param: context: The function context, including credential and runtime info.
 
-  evt = evt_lst['events'][0]
-  bucket_name = evt['oss']['bucket']['name']
-  endpoint = 'oss-' +  evt['region'] + '-internal.aliyuncs.com'
-  bucket = oss2.Bucket(auth, endpoint, bucket_name)
-  object_name = evt['oss']['object']['key']
+        For detail info, please refer to https://help.aliyun.com/document_detail/56316.html#using-context
+    """
+    evt_lst = json.loads(event)
+    creds = context.credentials
+    auth = oss2.StsAuth(
+        creds.access_key_id,
+        creds.access_key_secret,
+        creds.security_token)
 
-  if "ObjectCreated:PutSymlink" == evt['eventName']:
-    object_name = bucket.get_symlink(object_name).target_key
-    if object_name == "":
-      raise RuntimeError('{} is invalid symlink file'.format(evt['oss']['object']['key']))
+    evt = evt_lst['events'][0]
+    bucket_name = evt['oss']['bucket']['name']
+    endpoint = 'oss-' + evt['region'] + '-internal.aliyuncs.com'
+    bucket = oss2.Bucket(auth, endpoint, bucket_name)
+    object_name = evt['oss']['object']['key']
 
-  file_type = os.path.splitext(object_name)[1]
+    if "ObjectCreated:PutSymlink" == evt['eventName']:
+        object_name = bucket.get_symlink(object_name).target_key
+        if object_name == "":
+            raise RuntimeError('{} is invalid symlink file'.format(
+                evt['oss']['object']['key']))
 
-  if file_type != ".zip":
-    raise RuntimeError('{} filetype is not zip'.format(object_name))
+    file_type = os.path.splitext(object_name)[1]
 
-  LOGGER.info("start to decompress zip file = {}".format(object_name))
-  
-  lst = object_name.split("/")
-  zip_name = lst[-1]
-  PROCESSED_DIR = os.environ.get("PROCESSED_DIR", "")
-  if PROCESSED_DIR and PROCESSED_DIR[-1] != "/":
-    PROCESSED_DIR += "/"
-  newKey = PROCESSED_DIR + zip_name
-  zip_fp = helper.OssStreamFileLikeObject(bucket, object_name)
-  
-  newKey = newKey.replace(".zip", "/")
-  
-  with helper.zipfile_support_oss.ZipFile(zip_fp) as zip_file:
-    for name in zip_file.namelist():
-      with zip_file.open(name) as file_obj:
-        try:
-          name = name.encode(encoding='cp437')
-        except:
-          name = name.encode(encoding='utf-8')
-        
-        # the string to be detect is long enough, the detection result accuracy is higher 
-        detect = chardet.detect( (name*100)[0:100] )
-        confidence = detect["confidence"]
-        if confidence > 0.8:
-          try:
-            name = name.decode(encoding=detect["encoding"])
-          except:
-            name = name.decode(encoding='gb2312')
-        else:
-          name = name.decode(encoding="gb2312")
-          
-        bucket.put_object(newKey +  name, file_obj)
+    if file_type != ".zip":
+        raise RuntimeError('{} filetype is not zip'.format(object_name))
+
+    LOGGER.info("start to decompress zip file = {}".format(object_name))
+
+    lst = object_name.split("/")
+    zip_name = lst[-1]
+    PROCESSED_DIR = os.environ.get("PROCESSED_DIR", "")
+    if PROCESSED_DIR and PROCESSED_DIR[-1] != "/":
+        PROCESSED_DIR += "/"
+    newKey = PROCESSED_DIR + zip_name
+    zip_fp = helper.OssStreamFileLikeObject(bucket, object_name)
+
+    newKey = newKey.replace(".zip", "/")
+
+    with helper.zipfile_support_oss.ZipFile(zip_fp) as zip_file:
+        for name in zip_file.namelist():
+            with zip_file.open(name) as file_obj:
+                try:
+                    name = name.encode(encoding='cp437')
+                except:
+                    name = name.encode(encoding='utf-8')
+
+                # the string to be detect is long enough, the detection result accuracy is higher
+                detect = chardet.detect((name*100)[0:100])
+                confidence = detect["confidence"]
+                if confidence > 0.8:
+                    try:
+                        name = name.decode(encoding=detect["encoding"])
+                    except:
+                        name = name.decode(encoding='gb2312')
+                else:
+                    name = name.decode(encoding="gb2312")
+
+                bucket.put_object(newKey + name, file_obj)
